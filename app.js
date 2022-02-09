b8bd2ccb21b4eb617c5bf924d2927f2cc85d02e9
Jonathan Rebouças <jonathan.reboucas@toroinvestimentos.com.br>
2022-02-09 16:25:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const dotenv = require('dotenv')
+const venom = require('venom-bot');
+const express = require("express");
+const app = express();
+dotenv.config();
+let venomClient = null;
+
+app.use(express.json());
+
+const verifyXapiKey = async (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
+
+  if (!apiKey || apiKey == '')
+    res.status(401).send();
+
+  if (apiKey != process.env.APIKEY)
+    res.status(401).send();
+
+  next();
+}
+
+const verifyIfVenonIsInjected = async (req, res, next) => {
+
+  if (venomClient == null || venom == undefined)
+    res.status(500).send({ error: 'Venom-bot not injetected' });
+
+  next();
+}
+
+app.post('/send-message', verifyXapiKey, verifyIfVenonIsInjected, (req, res) => {
+  const { to, message } = req.body;
+
+  if(!to || !message || to == '' || message == '')
+  res.status(400).send();
+
+  if(!to.includes('@c.us'))
+  res.status(400).send({error:{
+    message:'Phone not valid',
+    example:'556100000000@c.us'
+  }});
+  try {
+
+    venomClient.sendText(to, message);
+    res.status(201).send({});
+
+  } catch (error) {
+    res.status(500).send(error);
+  }
+})
+
+app.listen(process.env.PORT, async () => {
+  console.log(`listening at the door ${process.env.PORT}`);
+
+  await initVenon();
+});
+
+const initVenon = async () => {
+  venomClient = await venom.create({
+    session: 'whatsapp-api', //name of session
+    multidevice: false, // for version not multidevice use false.(default: true)
+    createPathFileToken: true,
+    headless: true, // Headless chrome
+  }, (statusSession, session) => {
+
+    if (statusSession == 'notLogged')
+      initVenon();
+  });
+
+  await venomClient.getSessionTokenBrowser();
+
+}
